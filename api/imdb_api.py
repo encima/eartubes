9ebfe3f4c2d24d00df7662f10d84ed0ce63f854c
encima <encima@gmail.com>
2013-04-20 10:40:26 +0000

    added IMDB api and corrected Tastekid issues

---
+++ api/imdb_api.py
@@ -0,0 +1,27 @@
+import urllib2
+import json
+
+class IMDBApi:
+  def __init__(self):
+    self.API_ROOT = "http://imdbapi.org/"
+
+  def get_info(self, movie=None, year=None, limit=1):
+    params = {}
+    params['title'] = movie
+    if year != None:
+      params['year'] = year
+    params['limit'] = limit
+    return self.api_call(params)
+
+  #api calls to IMDB
+  def api_call(self, params):
+    paramString = "?type=json"
+    for param in params:
+      paramString += "&" + param + "=" + str(params[param])
+    request = self.API_ROOT + "" + paramString
+    print "REQUEST:" + request
+    response = urllib2.urlopen(request).read()
+    return json.dumps(response)    
+
+ia = IMDBApi()
+print ia.get_info('vanilla', None, 1)
---
+++ tastekid_api.py
@@ -0,0 +1,36 @@
+import urllib2
+import hashlib
+import json
+
+class TastekidApi:
+  def __init__(self, artist):
+    self.API_ROOT = "http://www.tastekid.com/ask/ws"
+    self.API_APP = "eartubes1763"
+    self.API_KEY = "mzqzmzg0mmnh"
+
+  def get_similar_movies_from_artists(self, artists):
+    artists = artists.split(", ")
+    params = {}
+    for artist in artists:
+      artist = artist.replace(" ", "+")
+      params[artist] = 'music'
+    return self.api_call(params, 'movies', False)
+
+
+  # make call to tastekid
+  def api_call(self, params, returnType, verbose):
+    paramString = ""
+    for param in params:
+      print param
+      paramString += params[param] + ":" + param + ","
+    #authenticate
+    paramString += "&f=" + self.API_APP + "&k=" + self.API_KEY
+    if verbose:
+      paramString += "&verbose=1"
+    request = self.API_ROOT + "?q=" + paramString + "//" + returnType  + "&format=JSON"
+    print "REQUEST:" + request
+    response = urllib2.urlopen(request).read()
+    return json.dumps(response)
+
+t = TastekidApi('hans+zimmer')
+print t.get_similar_movies_from_artists('hans zimmer, hank williams')
