a884195c63d1a348e13b085d8fe826911c864562
Will Webberley <willwebberley@gmail.com>
2013-04-20 16:24:40 +0100

    more ui updates

--- web.py
+++ web.py
@@ -82,7 +82,7 @@ def login():
         return json.dumps({'success':True})
     return json.dumps({'success':False, 'error':error})
 
-@app.route('/logout')
+@app.route('/logout/')
 def logout():
     session.pop('id', None)
     flash('You were logged out')
